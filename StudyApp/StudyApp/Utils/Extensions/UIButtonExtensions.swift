d556b5eadcb72fdbcca84d5ab9a324c179f9eab0
Valeriya Chubakova <valeri160500@gmail.com>
2021-04-22 16:34:49 +0300

    Appearance changes

---
+++ StudyApp/StudyApp/Utils/Extensions/UIButtonExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIButtonExtensions.swift
+//  StudyApp
+//
+//  Created by user on 4/22/21.
+//
+
+import UIKit
+
+extension UIButton {
+    func customButton () {
+        self.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        self.layer.cornerRadius = self.frame.height / 4
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = UIColor.white.cgColor
+    }
+}
