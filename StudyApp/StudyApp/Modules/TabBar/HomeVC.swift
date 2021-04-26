cb53c0ccdf6068423b3761120c86e432cbe2fc2b
Valeriya Chubakova <valeri160500@gmail.com>
2021-04-26 09:04:45 +0300

    Make base screens (Home, Settings)

--- StudyApp/StudyApp/Modules/TabBar/HomeVC.swift
+++ StudyApp/StudyApp/Modules/TabBar/HomeVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
