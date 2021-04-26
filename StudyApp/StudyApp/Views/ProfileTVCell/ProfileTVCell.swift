70e5ca51c9c4ecbc890eaa4b78677d7995068250
Valeriya Chubakova <valeri160500@gmail.com>
2021-04-26 10:36:58 +0300

    Change structure of project (add table view sell files)

---
+++ StudyApp/StudyApp/Views/ProfileTVCell/ProfileTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileTVCell.swift
+//  StudyApp
+//
+//  Created by user on 4/26/21.
+//
+
+import UIKit
+
+class ProfileTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ StudyApp/StudyApp/Views/SettingTVCell/SettingTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTVCell.swift
+//  StudyApp
+//
+//  Created by user on 4/26/21.
+//
+
+import UIKit
+
+class SettingTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
