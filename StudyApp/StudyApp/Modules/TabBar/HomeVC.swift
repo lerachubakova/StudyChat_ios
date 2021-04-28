c6832d4be347786a96dba666984066411f89b8f4
Valeriya Chubakova <valeri160500@gmail.com>
2021-04-28 15:34:32 +0300

    End message text view

--- StudyApp/StudyApp/Modules/TabBar/HomeVC.swift
+++ StudyApp/StudyApp/Modules/TabBar/HomeVC.swift
@@ -10,7 +10,7 @@ import UIKit
 class HomeVC: BaseVC {
 
     // MARK: - @IBOutlets
-    @IBOutlet weak private var myBackgroundImage: UIImageView!
+    @IBOutlet weak private var myBackgroundImageView: UIImageView!
     @IBOutlet weak private var navigationBarView: UIView!
     @IBOutlet weak private var navigationBar: UINavigationBar!
     @IBOutlet weak private var profileImageView: UIImageView!
@@ -22,13 +22,19 @@ class HomeVC: BaseVC {
     @IBOutlet weak private var addFileButton: UIButton!
     
     private var oldElementsPositions: [CGFloat] = []
+    private var inset = CGFloat()
+    
+    private let placeholderString = "Message..."
+    private let placeholderColor = UIColor.darkGray
+    private let textColor = UIColor.white
+    private let textViewMaxHeight: CGFloat = 141.6
 
     // MARK: - LifeCycle
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        navigationBar.transparent()
-        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
-        oldElementsPositions = [navigationBar, navigationBarView, profileImageView, myBackgroundImage].map({ $0?.frame.origin.y ?? 0.0 })
+        configureAppearance()
+        oldElementsPositions = [navigationBar, navigationBarView, profileImageView, myBackgroundImageView].map({ $0?.frame.origin.y ?? 0.0 })
+        moveElemets(on: inset)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -39,36 +45,52 @@ class HomeVC: BaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         addKeyboardObservers()
+        configureMessageTextView()
     }
     
     // MARK: - Logic
     private func addKeyboardObservers() {
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     private func moveElemets(on inset: CGFloat) {
-        self.navigationBar.frame.origin.y = oldElementsPositions[0] + inset
-        self.navigationBarView.frame.origin.y = oldElementsPositions[1] + inset
-        self.profileImageView.frame.origin.y = oldElementsPositions[2] + inset
-        self.myBackgroundImage.frame.origin.y = oldElementsPositions[3] + inset
-        self.view.frame.origin.y = -inset
+        navigationBar.frame.origin.y = oldElementsPositions[0] + inset
+        navigationBarView.frame.origin.y = oldElementsPositions[1] + inset
+        profileImageView.frame.origin.y = oldElementsPositions[2] + inset
+        myBackgroundImageView.frame.origin.y = oldElementsPositions[3] + inset
     }
-        
-    // MARK: - @IBAction
+    
+    private func configureAppearance() {
+        navigationBar.transparent()
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+    }
+    
+    private func configureMessageTextView() {
+        messageTextView.delegate = self
+        messageTextView.layer.borderColor = separatorColor.cgColor
+        messageTextView.text = placeholderString
+        messageTextView.textColor = placeholderColor
+        messageTextView.textContainerInset.left += 5.0
+        messageTextView.textContainerInset.right += 5.0
+    }
+
+    // MARK: - @IBActions
     @IBAction private func keyboardWillShow(notification: NSNotification) {
         guard let userInfo = notification.userInfo else { return }
         guard let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
 
         let bottomOfTextField = messageView.convert(messageView.bounds, to: self.view).maxY
         let topOfKeyboard = self.view.frame.height - keyboardFrame.height
-        let inset = bottomOfTextField - topOfKeyboard
-        
+        inset = bottomOfTextField - topOfKeyboard
         moveElemets(on: inset)
+        self.view.frame.origin.y = -inset
     }
     
     @IBAction private func keyboardWillHide(notification: NSNotification) {
-        moveElemets(on: 0.0)
+        inset = 0.0
+        moveElemets(on: inset)
+        self.view.frame.origin.y = inset
     }
     
     @IBAction private func sendMessage(_ sender: UIButton) {
@@ -79,4 +101,47 @@ class HomeVC: BaseVC {
         self.view.endEditing(true)
     }
     
+}
+
+// MARK: - Extensions
+extension HomeVC: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText: String = textView.text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+
+        if updatedText.isEmpty {
+            textView.text = placeholderString
+            textView.textColor = placeholderColor
+            textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+        } else if textView.textColor == placeholderColor && !text.isEmpty {
+            textView.textColor = textColor
+            textView.text = text
+        } else { return true }
+        
+        return false
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if self.view.window != nil {
+            if textView.textColor == placeholderColor {
+                textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+            } else {
+                let width = textView.frame.size.width
+                let newSize = textView.sizeThatFits(CGSize(width: width, height: CGFloat.greatestFiniteMagnitude))
+                let height = newSize.height
+                if height < textViewMaxHeight {
+                    messageTextConstraint.constant = height
+                    textView.isScrollEnabled = false
+                } else {
+                    textView.isScrollEnabled = true
+                    textView.showsVerticalScrollIndicator = true
+                    textView.indicatorStyle = .white
+                }
+            }
+        }
+    }
 }
